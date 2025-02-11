61f33175cb997f7171e804a485befa91c6af242e
thevowels <aungphyohtet1@gmail.com>
2025-02-11 10:35:31 +0700

    dd

--- app/Models/People.php
+++ app/Models/People.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class People extends Model
@@ -22,7 +23,11 @@ class People extends Model
         return $this->belongsTo(User::class,'user_id');
     }
 
-    
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class);
+    }
+
     public function getRouteKeyName(): string
     {
         return 'id';
