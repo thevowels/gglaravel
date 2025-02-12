36dbbd5ea97e8ba4abbfefcfde3dea543e5f3abd
thevowels <aungphyohtet1@gmail.com>
2025-02-12 10:25:04 +0700

    Adding note column on transactions table making migration with Schema:table udpate

--- resources/js/Pages/People/Partials/PeopleCard.tsx
+++ resources/js/Pages/People/Partials/PeopleCard.tsx
@@ -8,7 +8,6 @@ import { FormEventHandler, useState } from "react";
 import { useRoute } from "../../../../../vendor/tightenco/ziggy/src/js";
 export default function PeopleCard({person}:{person:any}){
 
-    console.log(person);
     const transactions:any = person.transactions;
     const accumulatedResult = transactions.reduce((a:any,b:any)=> {
         if(b.loan){
